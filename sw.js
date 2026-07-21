f2b8aba1a2983b0d40e9a9c2cb90f170fbb077c3
app34 <shafeeu038@gmail.com>
2026-07-21 08:44:29 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ============================================
 // SERVICE WORKER - OBLU SELECT SANGELI
-// Version: v4.0.3 | Build: 2026-07-21
+// Version: v4.0.5 | Build: 2026-07-21
 // ============================================
 
 const CACHE_NAME = 'oblu-sangeli-hk-cache-v6'; // INCREMENTED VERSION
